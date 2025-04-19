2f6dab544da55d26c6889cd16f90e9ade2bd0b5b
jtaylor05 <jacksontay666@gmail.com>
2025-04-19 19:03:36 -0400

    blocking out model code

---
+++ src/prompting_strategies.py
@@ -0,0 +1,12 @@
+def zero_shot_prompt():
+    pass
+def few_shot_prompt():
+    pass
+def chain_thought_prompt():
+    pass
+def self_consistency_prompt():
+    pass
+def role_play_prompt():
+    pass
+def prompt_chaining():
+    pass
